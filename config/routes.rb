21d8b0e805340131c67e728dc1d9289ccc9b9b68
nbknight <nicholas.b.knight@gmail.com>
2019-04-23 22:58:02 -0400

    //Created nested dashboard for admin //Created additional controllers and used resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 Rails.application.routes.draw do
 
+  # localhost3000/admin/dashboard/main is what I want the route to look like
+
+
+  namespace :admin do
+    get 'dashboard/main'
+    get 'dashboard/user'
+    get 'dashboard/blog'
+  end
+
+
+
+
+
   get 'about', to: 'pages#about'
 
   get 'leadgen/advertising/landingpage/lead', to: 'pages#contact', as: 'lead'
 
   resources :blogs
+  resources :posts
+
+  root to: 'pages#home'
   
 end
